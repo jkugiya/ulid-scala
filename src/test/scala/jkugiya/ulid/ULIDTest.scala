a24da8fac9177317cb47ed4f7b857dd7a4eb9cd9
jkugiya <j.kugiya@gmail.com>
2019-09-03 21:49:52 +0900

    Added travis.yml (#1)
    * Added travis.yml
    
    * Fixed tests for old builds
    
    * Added coveralls

--- src/test/scala/jkugiya/ulid/ULIDTest.scala
+++ src/test/scala/jkugiya/ulid/ULIDTest.scala
@@ -10,21 +10,32 @@ class ULIDTest extends FlatSpec with Matchers {
   def ulid(timestamp: Long, randomness: Array[Byte]): ULID =
     new ULID(timestamp, randomness)
 
+  private def nextBytes(n: Int = 10) = {
+    val bytes = new Array[Byte](0 max n)
+    Random.nextBytes(bytes)
+    bytes
+  }
+  private def nextLong(): Long = {
+    val value = Random.nextLong()
+    if (value < 0 || value >= ULID.MaxTimestamp) nextLong()
+    else value
+  }
+
   "first 48bit" should "be generated from timestamp" in {
-    val randomness = Random.nextBytes(10)
+    val randomness = nextBytes()
     ulid(ULID.MinTimestamp, randomness).base32.take(10) shouldBe "0" * 10
     ulid(1L, randomness).base32.take(10) shouldBe "0" * 9 + "1"
     ulid(ULID.MaxTimestamp, randomness).base32.take(10) shouldBe "7ZZZZZZZZZ"
   }
   "last 80bit" should "be generated from randomness" in {
-    val timestamp = Random.nextLong(ULID.MaxTimestamp)
+    val timestamp = nextLong()
     ulid(timestamp, Array.fill(10)(0)).base32.takeRight(16) shouldBe "0" * 16
     ulid(timestamp, Array.fill(10)(-1)).base32.takeRight(16) shouldBe "Z" * 16
   }
   it should "be failed" in {
-    an[IllegalArgumentException] should be thrownBy ulid(ULID.MinTimestamp - 1, Random.nextBytes(10))
-    an[IllegalArgumentException] should be thrownBy ulid(ULID.MaxTimestamp + 1, Random.nextBytes(10))
-    an[IllegalArgumentException] should be thrownBy ulid(ULID.MinTimestamp, Random.nextBytes(9))
-    an[IllegalArgumentException] should be thrownBy ulid(ULID.MinTimestamp, Random.nextBytes(11))
+    an[IllegalArgumentException] should be thrownBy ulid(ULID.MinTimestamp - 1, nextBytes())
+    an[IllegalArgumentException] should be thrownBy ulid(ULID.MaxTimestamp + 1, nextBytes())
+    an[IllegalArgumentException] should be thrownBy ulid(ULID.MinTimestamp, nextBytes(9))
+    an[IllegalArgumentException] should be thrownBy ulid(ULID.MinTimestamp, nextBytes(11))
   }
 }
